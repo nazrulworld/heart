21cdd24abf95e1bfd90ef2bbe4d4e30a34d6f781
Md Nazrul Islam <connect2nazrul@gmail.com>
2018-12-15 20:54:15 +0100

    Back to development: 0.1.0a1

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
       'test': test_requirements + setup_requirements
     },
     url='https://github.com/nazrulworld/heart',
-    version='0.1.0',
+    version='0.1.0a1.dev0',
     zip_safe=False,
 )
